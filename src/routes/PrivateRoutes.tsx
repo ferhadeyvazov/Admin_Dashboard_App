776d59e9e59eb5ce6df809baeb8826a9e601d1df
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2025-01-10 13:45:40 +0400

    Some Dashbboard components was added

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -8,11 +8,22 @@ import {
 } from "react-router-dom"
 // PAGES
 import Dashboard from "../pages/dashboard/Dashboard"
-
+import Team from '../components/private/team/Team'
+import Contacts from '../components/private/contacts/Contacts'
+import Bar from '../components/private/bar/Bar'
+import Form from '../components/private/form/Form'
+import Calendar from '../components/private/calendar/Calendar'
+import Faq from '../components/private/faq/Faq'
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/'>
             <Route index element={<Dashboard />} />
+            <Route path='team' element={<Team />} />
+            <Route path='contacts' element={<Contacts />} />
+            <Route path='bar' element={<Bar />} />
+            <Route path='form' element={<Form />} />
+            <Route path='calendar' element={<Calendar />} />
+            <Route path='faq' element={<Faq />} />
             <Route path='*' element={<Navigate to={"/"} />} />
         </Route>
     )
