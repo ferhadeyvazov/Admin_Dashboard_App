5d5ca4e1ea77f195c18d81205d5894dbe7673ca8
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2025-03-17 23:36:54 +0400

    StatBox.tsx is ready

---
+++ src/features/StatBox.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Box, useTheme, Typography } from '@mui/material'
+import { tokens } from '../redux/reducer/theme/Theme'
+import ProgressCircle from './ProgressCircle'
+
+type IProps = {
+    title: string;
+    subtitle: string;
+    icon: React.ReactNode;
+    progress: number;
+    increase: string;
+}
+const StatBox: React.FC<IProps> = ({ title, subtitle, icon, progress, increase }) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return (
+        <Box width="100%" m="0 30px">
+            <Box display="flex" justifyContent="space-between">
+                <Box>
+                    {icon}
+                    <Typography
+                        variant='h4'
+                        fontWeight="bold"
+                        color={colors.grey[900]}
+                    >
+                        {title}
+                    </Typography>
+                </Box>
+                <Box>
+                    <ProgressCircle progress={progress} />
+                </Box>
+            </Box>
+
+            <Box display="flex" justifyContent="space-between" mt="2px">
+                <Typography
+                    variant='h5'
+                    color={colors.greenAccent[500]}
+                >
+                    {subtitle}
+                </Typography>
+                <Typography
+                    variant='h5'
+                    fontStyle="italic"
+                    color={colors.greenAccent[400]}
+                >
+                    {increase}
+                </Typography>
+            </Box>
+        </Box>
+    )
+}
+
+export default StatBox
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,14 +1,68 @@
+import {
+  DownloadOutlined,
+  EmailOutlined
+} from '@mui/icons-material'
+import { Box, Button, useTheme } from '@mui/material'
 import React from 'react'
 import MainLayout from '../../layouts/MainLayout'
 import Title from '../../layouts/Title'
-import { Box } from '@mui/material'
+import { tokens } from '../../redux/reducer/theme/Theme'
+import StatBox from '../../features/StatBox'
 
 const Dashboard: React.FC = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
     <MainLayout>
-      <Box my='1.25rem'>
+      {/* BOX TITLE */}
+      <Box
+        display='flex'
+        justifyContent='space-between'
+        alignItems='center'
+      >
         <Title title='Dashboard' subtitle='Welcome your dashboard' />
+        <Button
+          sx={{
+            backgroundColor: colors.blueAccent[700],
+            color: colors.grey[100],
+            fontSize: '14px',
+            fontWeight: 'bold',
+            padding: "10px 20px"
+          }}
+        >
+          <DownloadOutlined sx={{ mr: '10px' }} />
+          Download Reports
+        </Button>
       </Box>
+
+      <Box mt="40px" height="75vh" width="100%">
+        {/* GRID and CHARTS */}
+        <Box
+          display='grid'
+          gridTemplateColumns="repeat(12, 1fr)"
+          gridAutoRows='140px'
+          gap='20px'
+        >
+          {/* ROW - 1 */}
+          <Box gridColumn="span 3" bgcolor={colors.primary[400]}>
+            <StatBox
+            title='12,361'
+            subtitle="Emails Sent"
+            progress={0.75}
+            increase="+14%"
+            icon={
+              <EmailOutlined
+                sx={{ 
+                  color:colors.greenAccent[400],
+                  fontSize: '26px'
+                }}/>
+            }
+            />
+          </Box>
+
+        </Box>
+      </Box>
+
     </MainLayout>
   )
 }
